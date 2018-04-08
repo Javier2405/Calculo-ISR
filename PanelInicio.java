0a3e46fac6d5956e7aa97f73c430a49a26451631
Victor Javier Aguayo Mendoza <a01229260@itesm.mx>
2018-04-08 16:44:27 -0500

    Update PanelInicio.java

--- PanelInicio.java
+++ PanelInicio.java
@@ -1,7 +1,8 @@
 import java.awt.Dimension;
+import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+import javax.swing.*;
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -14,17 +15,20 @@ public class PanelInicio extends JPanel{
 	private JRadioButton rbIndividual,
 						 rbDocumento;
 	private VentanaPrograma vp;
+	private VentanaProgramaDoc vpd;
+	private JLabel etiqueta;
 	
 	
 	public PanelInicio() {
 		super();
-		this.setPreferredSize(new Dimension(500,300));
+		this.setPreferredSize(new Dimension(600,200));
 		
 		this.btAceptar = new JButton("Aceptar");
 		
 		this.rbIndividual = new JRadioButton("Realizar el calculo para una sola persona");
 		this.rbDocumento = new JRadioButton("Realizar el calculo para varias personas por medio de un documento .csv");
 		this.rbIndividual.setSelected(true);
+		this.etiqueta = new JLabel("Selecciona la opcion que desees");
 		
 		ButtonGroup bg = new ButtonGroup();
 		bg.add(this.rbIndividual);
@@ -36,18 +40,24 @@ public class PanelInicio extends JPanel{
 					vp = new VentanaPrograma();
 				}
 				else if(rbDocumento.isSelected()) {
-					System.out.println("ya no hay nada");
+					vpd = new VentanaProgramaDoc();
 					
 				}	
 			}
 
 		});
 		
-		this.add(new JLabel("Seleccione la opcion que desee"));
-		this.add(this.rbIndividual);
-		this.add(this.rbDocumento);
-		this.add(this.btAceptar);
+		this.rbIndividual.setBounds(50,50,450,30);
+		this.rbDocumento.setBounds(50,100,450,30);
+		this.btAceptar.setBounds(250,150,100,30);
+		this.etiqueta.setBounds(50,0,250,30);
 		
+		this.setLayout(null);
+		
+		this.add(this.rbDocumento);
+		this.add(this.rbIndividual);
+		this.add(this.btAceptar);
+		this.add(this.etiqueta);
 		
 	}
 }
