58db61b6224b801e2a80ae75d4ad25ac856ec600
Victor Javier Aguayo Mendoza <a01229260@itesm.mx>
2018-04-10 19:17:03 -0500

    Update VentanaProgramaDoc.java

--- VentanaProgramaDoc.java
+++ VentanaProgramaDoc.java
@@ -1,3 +1,5 @@
+import java.awt.Dimension;
+import java.awt.Toolkit;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import javax.swing.JFrame;
@@ -5,16 +7,21 @@ import javax.swing.JFrame;
 public class VentanaProgramaDoc extends JFrame{
 	public VentanaProgramaDoc() {
 		super();
+		Dimension pantalla = Toolkit.getDefaultToolkit().getScreenSize();
+
+	    this.setLocation(
+	            (pantalla.width-600) / 2,
+	            (pantalla.height-200) / 2);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		PanelDocumento pdoc=new PanelDocumento();
 		this.add(pdoc);
 		this.pack();
 		this.setVisible(true);
-		/*this.addWindowListener(new WindowAdapter() {
+		this.addWindowListener(new WindowAdapter() {
 			public void windowDeactivated(WindowEvent e) {
 				dispose();
 			}
-		});*/
+		});
 	}
 	public static void main(String[] args) {
 		VentanaProgramaDoc a=new VentanaProgramaDoc();
