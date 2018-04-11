356372609ed502bc2485c5590c2f187a84a75151
Victor Javier Aguayo Mendoza <a01229260@itesm.mx>
2018-04-10 19:18:15 -0500

    Update VentanaInicio.java

--- VentanaInicio.java
+++ VentanaInicio.java
@@ -1,5 +1,5 @@
-
-
+import java.awt.Dimension;
+import java.awt.Toolkit;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -8,11 +8,17 @@ import javax.swing.JFrame;
 public class VentanaInicio extends JFrame {
 	public VentanaInicio() {
 		super();
+		Dimension pantalla = Toolkit.getDefaultToolkit().getScreenSize();
+
+	    this.setLocation(
+	            (pantalla.width-600) / 2,
+	            (pantalla.height-200) / 2);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		PanelInicio pi=new PanelInicio();
 		this.add(pi);
 		this.pack();
 		this.setVisible(true);
+		
 		this.addWindowListener(new WindowAdapter() {
 			public void windowDeactivated(WindowEvent e) {
 				dispose();
@@ -24,5 +30,3 @@ public class VentanaInicio extends JFrame {
 	}
 
 }
-
-
