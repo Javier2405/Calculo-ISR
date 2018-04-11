b8835665b89df494e9210933476489136a626628
Victor Javier Aguayo Mendoza <a01229260@itesm.mx>
2018-04-10 19:20:52 -0500

    Update PanelCalculo.java

--- PanelCalculo.java
+++ PanelCalculo.java
@@ -4,65 +4,71 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.StringTokenizer;
-
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
 
 public class PanelCalculo extends JPanel implements ActionListener{
+	
 	private CalculosISR Cal;
+	
 	private StatusBar sb;
-	private JTextField nombre;
-	private JTextField RFC;
-	private JTextField SueldoM;
-	private JTextField Aguinaldo;
-	private JTextField PrimaV;
-	private JTextField DMyHosp;
-	private JTextField GastosFun;
-	private JTextField SGMM;
-	private JTextField Hipotecarios;
-	private JTextField Donativos;
-	private JTextField AportacionRet;
-	private JTextField TransporteE;
-	private JRadioButton Preescolar;
-	private JRadioButton Primaria;
-	private JRadioButton Secundaria;
-	private JRadioButton ProfesionalTec;
-	private JRadioButton Bachillerato;
-	private JTextField ColegiaturaTot;
-	private JButton Submit;
-	private JButton Abrir;
-	private JButton Guardar;
-	private JButton Regresar;
-	private String NivelE;
-	private JButton Limpiar;
 	
-
+	private JTextField nombre,
+						RFC,
+						SueldoM,
+						Aguinaldo,
+						PrimaV,
+						DMyHosp,
+						GastosFun,
+						SGMM,
+						Hipotecarios,
+						Donativos,
+						AportacionRet,
+						TransporteE,
+						ColegiaturaTot;
+	
+	private JRadioButton Preescolar,
+							Primaria,
+							Secundaria,
+							ProfesionalTec,
+							Bachillerato;
+	private JButton Submit,
+					Abrir,
+					Guardar,
+					Regresar,
+					Limpiar;
+	
+	private String NivelE,
+					currentLine;
+	
 	private JFileChooser fileChooser;
+	
 	private BufferedReader br;
+	
 	int returnVal;
+	
 	private File file;
-	String currentLine;
-	
-	
 	
 	
 	public PanelCalculo(CalculosISR Cal, StatusBar sb) {
 		super();
-		this.Cal=Cal;
-		this.sb=sb;
 		this.setPreferredSize(new Dimension(250,500));
 		this.setBackground(Color.LIGHT_GRAY);
-		this.NivelE="Primaria";
+		
+		this.Cal=Cal;
+		this.sb=sb;
+		
 		this.nombre=new JTextField(15);
 		this.RFC=new JTextField(15);
 		this.SueldoM=new JTextField(7);
@@ -75,24 +81,30 @@ public class PanelCalculo extends JPanel implements ActionListener{
 		this.Donativos=new JTextField(10);
 		this.AportacionRet=new JTextField(6);
 		this.TransporteE=new JTextField(5);
+		this.ColegiaturaTot=new JTextField(5);
+		
+		
 		this.Preescolar=new JRadioButton("Preescolar");
 		this.Primaria=new JRadioButton("Primaria");
 		this.Secundaria=new JRadioButton("Secundaria");
 		this.ProfesionalTec=new JRadioButton("Profesional Tecnico");
 		this.Bachillerato=new JRadioButton("Bachillerato o equivalente");
+		
 		ButtonGroup bg=new ButtonGroup();
 		bg.add(this.Preescolar);
 		bg.add(this.Primaria);
 		bg.add(this.Secundaria);
 		bg.add(this.ProfesionalTec);
 		bg.add(this.Bachillerato);
-		this.ColegiaturaTot=new JTextField(5);
+		this.Primaria.setSelected(true);
+		//JBUTTON
 		this.Abrir=new JButton("ABRIR DOCUMENTO .CSV");
 		this.Guardar=new JButton("GUARDAR");
 		this.Submit=new JButton("CALCULAR ISR");
 		this.Regresar=new JButton("Home");
 		this.Limpiar=new JButton("Limpiar");
 		
+		//LABELS
 		this.add(new JLabel("CALCULADORA ISR"));
 		this.add(new JLabel("                  ------------------------------------------------------------                 "));
 		this.add(new JLabel("                  LLENA LOS DATOS A CALCULAR                        "));
@@ -159,8 +171,9 @@ public class PanelCalculo extends JPanel implements ActionListener{
 				this.abrirDatos();
 				
 			}else if(e.getSource() == this.Regresar) {
+				JFrame frame = (JFrame)SwingUtilities.getRoot(this);
+				frame.dispose();
 				VentanaInicio vi = new VentanaInicio();
-
 			}else if(e.getSource()==this.Limpiar) {
 				this.Limpia();
 			}
@@ -190,24 +203,16 @@ public class PanelCalculo extends JPanel implements ActionListener{
 			this.NivelE="Preparatoria";
 		}
 		
-		
-		
-		
 		if(this.nombre.getText().equals("")){
 			this.nombre.setText("No name");
 		}
 		
 		if(this.RFC.getText().equals("")){
 			this.RFC.setText("No RFC dado");
-
 		}
-		
 		if(this.SueldoM.getText().equals("")){
 			this.SueldoM.setText("0");
-
 		}
-		
-		
 		if(this.Aguinaldo.getText().equals("")){
 			this.Aguinaldo.setText("0");
 		}
@@ -259,10 +264,7 @@ public class PanelCalculo extends JPanel implements ActionListener{
 		this.ColegiaturaTot.setText("");
 		
 	}
-
-
-
-
+	
 	private void saveFile() {
 		Cal.saveData();
 		this.sb.setMessage("Guardado exitosamente");
@@ -276,7 +278,7 @@ public class PanelCalculo extends JPanel implements ActionListener{
 			StringTokenizer st;
 			try{
 				br=new BufferedReader(new FileReader(file));
-				//br.readLine();
+				br.readLine();
 				currentLine=br.readLine();
 				st=new StringTokenizer(currentLine);
 				
@@ -285,81 +287,28 @@ public class PanelCalculo extends JPanel implements ActionListener{
 				String rfc=st.nextToken(",");//
 				
 				Double sueldo=(Double.parseDouble(st.nextToken(",")));
-				//int sueldoM= sueldo.intValue();//
-				
-				//Double ingreso=(Double.parseDouble(st.nextToken(",")));
-				//int ingresoA=ingreso.intValue();//
 				
 				Double Aguinaldo=(Double.parseDouble(st.nextToken(",")));
-				//int aguinaldo=Aguinaldo.intValue();
-				
-				//Double AguinaldoE=(Double.parseDouble(st.nextToken(",")));
-				//int aguinaldoE=AguinaldoE.intValue();
-				
-				//Double AguinaldoG=(Double.parseDouble(st.nextToken(",")));
-				//int aguinaldoG=AguinaldoG.intValue();
 				
 				Double Primav=(Double.parseDouble(st.nextToken(",")));
-				//int PrimaV=Primav.intValue();
-				
-				//Double PrimavE=(Double.parseDouble(st.nextToken(",")));
-				//int PrimaVE=PrimavE.intValue();
-				
-				//Double PrimavG=(Double.parseDouble(st.nextToken(",")));
-				//int PrimaVG=PrimavG.intValue();
-				
-				//Double TotalIG=(Double.parseDouble(st.nextToken(",")));
-				//int totalIG=TotalIG.intValue();
-				
+
 				Double MyH=(Double.parseDouble(st.nextToken(",")));
-				//int myh=MyH.intValue();
-				
+
 				Double GF=(Double.parseDouble(st.nextToken(",")));
-				//int Funerarios=GF.intValue();
-				
+
 				Double SGMM=(Double.parseDouble(st.nextToken(",")));
-				//int sgmm=SGMM.intValue();
-				
+
 				Double Hip=(Double.parseDouble(st.nextToken(",")));
-				//int hipotecarios=Hip.intValue();
-				
+
 				Double Donat=(Double.parseDouble(st.nextToken(",")));
-				//int Donativos=Donat.intValue();
-				
+
 				Double SubR=(Double.parseDouble(st.nextToken(",")));
-				//int subR=SubR.intValue();
-				
+
 				Double Transp=(Double.parseDouble(st.nextToken(",")));
-				//int transpE=Transp.intValue();
 				
 				String NivelE=st.nextToken(",");
 				
-				//Double MaxDedColeg=(Double.parseDouble(st.nextToken(",")));
-				//int maxdedcoleg=MaxDedColeg.intValue();
-				
 				Double Colegiatura=(Double.parseDouble(st.nextToken(",")));
-				//int ColegiaturaP=Colegiatura.intValue();
-				
-				//Double Totald=(Double.parseDouble(st.nextToken(",")));
-				//int totalD=Totald.intValue();
-				
-				//Double DeducP=(Double.parseDouble(st.nextToken(",")));
-				//int deducP=DeducP.intValue();
-				
-				//Double MontoISR=(Double.parseDouble(st.nextToken(",")));
-				//int montoISR=MontoISR.intValue();
-				
-				//Double CuotaF=(Double.parseDouble(st.nextToken(",")));
-				//int CuotaFija=CuotaF.intValue();
-				
-				//Double PorcE=(Double.parseDouble(st.nextToken(",")));
-				//int PorcentE=PorcE.intValue();
-				
-				//Double pagoE=(Double.parseDouble(st.nextToken(",")));
-				//int PagoE=pagoE.intValue();
-				
-				//Double TotalP=(Double.parseDouble(st.nextToken(",")));
-				//int TotalPa=TotalP.intValue();
 				
 				Cal.abrir(name, rfc, sueldo, Aguinaldo, Primav, MyH, GF,
 						SGMM, Hip, Donat, SubR, Transp, NivelE, Colegiatura);
